c4b0421b1b68520ba5660a607b8425b5704d5011
learn <learn@learn14.attlocal.net>
2016-11-29 09:12:41 -0800

    log in tests

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -12,4 +12,63 @@ RSpec.feature "LandingPages", type: :feature do
       end
     end
   end
-end
+
+  context "Signing up" do
+    Steps "To sign up" do
+      Given "I am on the landing page" do
+        visit "/"
+      end
+        expect(page).to have_content("Sign Up")
+      And "I can go to the sign up page" do
+        click_link "Sign Up"
+      end
+      Then "I am taken to a sign up page" do
+        expect(page).to have_content("Sign up")
+      end
+      And "I can fill out the sign up form" do
+        fill_in "user_email", with: "a@a.com"
+        fill_in "user_password", with: "asdfgh"
+        fill_in "user_password_confirmation", with: "asdfgh"
+
+      end
+      Then "I can sign up do" do
+        click_button("Sign up")
+      end
+      And "I am taken to the landing page" do
+        expect(page).to have_content("Welcome! You have signed up successfully.")
+      end
+
+
+    end #steps
+  end #context
+
+  context "logging out" do
+    Steps "To log out a logged in user" do
+      Given "I am logged in" do
+        visit "/"
+        click_link "Sign Up"
+        fill_in "user_email", with: "a@a.com"
+        fill_in "user_password", with: "asdfgh"
+        fill_in "user_password_confirmation", with: "asdfgh"
+        click_button("Sign up")
+      end#given
+
+      And "I can no longer see a 'sign up' link" do
+        expect(page).to_not have_content('Sign up')
+      end
+
+      Then "I can see a log out link" do
+        expect(page).to have_content("Log Out")
+      end
+
+      Then "I can log out!" do
+        click_link "Log Out"
+      end
+
+
+    end#steps
+  end#context
+
+
+
+end #feature
