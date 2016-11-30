0ad153a11c8120494641c2d6e4a35c811a8979ed
mosclov <federico@grupofemal.com>
2016-11-30 13:35:59 -0800

    Review UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if @user.id != current_user.id
+      flash[:alert] = "Can't go into others profile!"
       redirect_to '/'
     end
   end
